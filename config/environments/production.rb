77b095e40a7ba23b51d852317629994a611199fe
jcheng77 <jackie.f.cheng@gmail.com>
2012-03-30 03:41:09 +0800

    deploy to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Tb::Application.configure do
+Ssg::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
