2a7930d22b222a52d062694ac3ed1d245b438cf3
jcheng77 <jackie.f.cheng@gmail.com>
2012-05-20 13:48:46 +0800

    add initializer for categories

---
+++ config/initializers/init_categories.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+Category.find_or_create_by cid: 1, cn: "数码"
+Category.find_or_create_by cid: 2, cn: "户外"
+Category.find_or_create_by cid: 3, cn: "男装"
+Category.find_or_create_by cid: 4, cn: "女装"
+Category.find_or_create_by cid: 5, cn: "饰品"
+Category.find_or_create_by cid: 6, cn: "化妆品"
+Category.find_or_create_by cid: 7, cn: "居家"
+Category.find_or_create_by cid: 8, cn: "其他"
+
