5b7121409a302f5403f48832fd1207315998b38e
jcheng77 <jackie.f.cheng@gmail.com>
2012-05-23 15:59:00 +0800

    add bookmarklet_helper

---
+++ app/lib/bookmarklet_helper.rb
@@ -0,0 +1,22 @@
+require 'nokogiri'
+require 'open-uri'
+
+module BookmarkletHelper
+
+  def taobao_collector(url)
+    doc = Nokogiri::HTML(open(url))
+    imgs = []
+    doc.css('div.tb-s310 img').each do |node|
+      node.values.each do |val|
+        imgs << val if val.match(/^http/)
+      end
+    end
+
+    doc.css('div.tb-s40 img').each do |node|
+      imgs << node.values.first
+    end
+
+    return imgs
+  end
+
+end
