a3607a3e128cb01136f891caad32c2fda8479e66
Jackie Cheng <jackie.f.cheng@gmail.com>
2012-08-22 21:08:23 +0800

    regenerate bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,20 +6,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//
-// Include all twitter's javascripts
-//= require bootstrap
-//= require bootstrap-transition
-//= require bootstrap-alert
-//= require bootstrap-modal
-//= require bootstrap-dropdown
-//= require bootstrap-scrollspy
-//= require bootstrap-tab
-//= require bootstrap-tooltip
-//= require bootstrap-popover
-//= require bootstrap-button
-//= require bootstrap-collapse
-//= require bootstrap-carousel
-//= require bootstrap-typeahead
-//
+//= require twitter/bootstrap
 //= require_tree .
