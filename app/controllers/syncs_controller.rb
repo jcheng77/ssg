059eaac5d052eb71eea9fde233057a5954e1d031
Jackie Cheng <jackie.f.cheng@gmail.com>
2012-09-01 23:05:27 +0800

    upgraded to sina weibo oauth2.0 successfully :-)

--- app/controllers/syncs_controller.rb
+++ app/controllers/syncs_controller.rb
@@ -1,3 +1,5 @@
+require 'oauth2'
+
 class SyncsController < ApplicationController
   layout 'application1'
   skip_before_filter :authenticate
@@ -6,18 +8,20 @@ class SyncsController < ApplicationController
   end
   
   def new
-    WeiboOAuth2::Config.api_key = '3788831273'
-    WeiboOAuth2::Config.api_secret = 'cd9072acaac30aaa6d7a45dc8fff57e3'
-    WeiboOAuth2::Config.redirect_uri = 'http://boluo.me/syncs/sina/callback/'
-    @client = WeiboOAuth2::Client.new
-
+    
     if params[:type] == 'qq'
     wb = Weibo.new(params[:type])
     wb.init_client
     wb.write_to_cache
     redirect_to wb.client.authorize_url
     else
-    redirect_to @client.authorize_url
+    #client = OAuth2::Client.new('3788831273', 'cd9072acaac30aaa6d7a45dc8fff57e3', :site => 'https://api.weibo.com', authorize_url: '/oauth2/authorize', token_url: '/oauth2/access_token')
+    WeiboOAuth2::Config.api_key = '3788831273'
+    WeiboOAuth2::Config.api_secret = 'cd9072acaac30aaa6d7a45dc8fff57e3'
+    WeiboOAuth2::Config.redirect_uri = 'http://www.boluome.com:3000/syncs/sina/callback/'
+    client = WeiboOAuth2::Client.new
+    #client.auth_code.authorize_url(:redirect_uri => 'http://www.boluome.com:3000/syncs/sina/callback/')
+    redirect_to client.authorize_url
     end
   end
 
@@ -40,11 +44,11 @@ class SyncsController < ApplicationController
       
     WeiboOAuth2::Config.api_key = '3788831273'
     WeiboOAuth2::Config.api_secret = 'cd9072acaac30aaa6d7a45dc8fff57e3'
-    WeiboOAuth2::Config.redirect_uri = 'http://boluo.me/syncs/sina/callback/'
-    @client = WeiboOAuth2::Client.new
-    code = @client.auth_code.get_token(params[:code])
-    puts code
-    redirect_to :controller => "users", :action => "signup" 
+    WeiboOAuth2::Config.redirect_uri = 'http://www.boluome.com:3000/syncs/sina/callback/'
+    client = WeiboOAuth2::Client.new
+    code = client.auth_code.get_token(params[:code])
+    userinfo = client.users.show_by_uid(code.params["uid"])
+
     end
 
     if (access_token && token_secret) || code
