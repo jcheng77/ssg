2cc94f70dc9b4d1371dba0ae53d9b9ce16ef7a55
Zernel <itzernel@gmail.com>
2012-09-03 22:04:01 +0800

    Move the js from categories/_category.html.erb to assets

---
+++ app/assets/javascripts/category.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".category li a").click(function () {
+    var url = $(location).attr("href");
+    var index = url.indexOf('?');
+    if(index != -1) {
+      url = url.substring(0, index);
+    }
+    var param = "?category=" + $(this).attr("value");
+    $(location).attr("href",  url + param);
+  });
+});
