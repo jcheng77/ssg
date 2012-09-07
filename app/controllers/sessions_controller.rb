6c99d438808fbaabf16a52ad5d755f2d7c8cdf7b
jcheng77 <jackie.f.cheng@gmail.com>
2012-09-08 01:04:48 +0800

    add qq weibo upload image with url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@
 class SessionsController < ApplicationController
   def destroy
     #Weibo.new('qq').add_status(session[:access_token],session[:token_secret],'Knowledge is power. Pass it on...')
-    #Weibo.new('sina').upload_image(session[:access_token],session[:token_secret],"my handwriting","/Users/jcheng/Pictures/head.jpg") 
+    Weibo.new('qq').upload_image_url(session[:access_token],session[:token_secret],"Classic PX200","http://product.it.sohu.com/img/product/picid/5168051.jpg")
     #Weibo.new('qq').upload_image(session[:access_token],session[:token_secret],'skydrive is fast','https://sc.imp.live.com/content/dam/imp/surfaces/mail_signin/v3/images/SignIn_SkyDrive_en.jpg')
     session[:current_user] = nil
     session[:current_user_id] = nil
