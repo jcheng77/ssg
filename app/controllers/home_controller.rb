18a7e7a342481227e88d29f45ce3ce7c93a77283
jcheng77 <jackie.f.cheng@gmail.com>
2012-09-12 15:45:18 +0800

    skip relogin if the session has not expired

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   layout 'application'
   skip_before_filter :authenticate
+
+  def index
+    if current_user
+        redirect_to dashboard_user_path(current_user)
+    else
+      redirect_to root_url
+    end
+  end
+
 end
