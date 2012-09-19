48265073d4475ff98d251fdef2f529a7496dbe25
Zernel <itzernel@gmail.com>
2012-09-19 23:07:39 +0800

    Add share_id to share_queue model

--- app/models/share_queue.rb
+++ app/models/share_queue.rb
@@ -4,6 +4,7 @@ class ShareQueue
   field :cmt, as: :share_comment, type:String
   field :url, as: :item_url, type:String
   field :wid, as: :weibo_status_id, type: String
+  field :sid, as: :share_id, type: BSON::ObjectId
 
   validates_uniqueness_of :wid
 end
