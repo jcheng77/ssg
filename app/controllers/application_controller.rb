6ca36280fb55aad9db09e299099c9d85ca1d2f63
jcheng77 <jackie.f.cheng@gmail.com>
2012-09-21 11:14:12 +0800

    change the weibo login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -86,8 +86,8 @@ class ApplicationController < ActionController::Base
   def weibo_client
     #sina weibo production test api client
 
-    #@client ||= (session[:client] || WeiboOAuth2::Client.new('1734028369', '281bbd8a50b59ce1cdadb9d5e8380ab1'))
-    #WeiboOAuth2::Config.redirect_uri = 'http://boluo.me/syncs/sina/callback/'
+    @client ||= (WeiboOAuth2::Client.new('1734028369', '281bbd8a50b59ce1cdadb9d5e8380ab1')
+    WeiboOAuth2::Config.redirect_uri = 'http://boluo.me/syncs/sina/callback/'
 
     #@client ||= ( session[:client] || WeiboOAuth2::Client.new( '3788831273','cd9072acaac30aaa6d7a45dc8fff57e3'))
     #WeiboOAuth2::Config.redirect_uri = 'http://boluo.me/syncs/sina/callback/'
@@ -95,8 +95,8 @@ class ApplicationController < ActionController::Base
     # client = WeiboOAuth2::Client.new( '419180446','8d97de6064802d452a721e9a64c82310')
     # WeiboOAuth2::Config.redirect_uri = 'http://boluo.me/syncs/sina/callback/'
 
-    @client ||= WeiboOAuth2::Client.new( '1408937818','613b940d9fe14180aa01ce294e1ddf8a')
-    WeiboOAuth2::Config.redirect_uri = 'http://127.0.0.1:3000/syncs/sina/callback/'
+    #@client ||= WeiboOAuth2::Client.new( '1408937818','613b940d9fe14180aa01ce294e1ddf8a')
+    #WeiboOAuth2::Config.redirect_uri = 'http://127.0.0.1:3000/syncs/sina/callback/'
 
     if !@client.authorized? && !session[:access_token].nil?
       @client.get_token_from_hash(:access_token => session[:access_token], :refresh_token => session[:refresh_token] , :expires_at => session[:expires_at] )
