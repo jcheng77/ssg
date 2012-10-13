51b50b34459c495c927861e56dd95d15f811c2a4
garyzhao <zl_demon@yahoo.com.cn>
2012-10-13 15:11:14 +0800

    pull items on items/index page

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,16 +1,37 @@
 $(document).ready(function () {
-    $(".category li a").click(function () {
-        var url = $(location).attr("href");
-        var index = url.indexOf('?');
-        if (index != -1) {
-            url = url.substring(0, index);
-        }
-
-        var action;
-        if ($(this).hasClass("checked")) {
-            action = "delete"
+    var selectCatHandler = function () {
+        var anchorVal = $(this).attr("value");
+        if(anchorVal == "expend"){
+            $("ul.category:eq(1)").fadeIn(500);
+            $(this).attr("value", "collapse").html("&lt;");
+        } else if (anchorVal == "collapse") {
+            $("ul.category:eq(1)").hide();
+            $(this).attr("value", "expend").html("&gt;");
+        } else if('extra' == $(this).attr("cat-type")){
+            $("ul.category:eq(0)").append("<li></li>");
+            var newAddCat = $(this).clone();
+            newAddCat.attr("href", newAddCat.attr("path-cat"));
+            newAddCat.attr("cat-type", "");
+            newAddCat.click(selectCatHandler);
+            $("ul.category:eq(0) li:last").append(newAddCat);
+            $(this).detach();
+            
+            if($("ul.category:eq(1) a").size() > 0) {
+                $("ul.category:eq(1)").hide();
+                $(".category li a[value=collapse]").attr("value", "expend").html("&gt;");    
+            } else {
+                $("ul.category:last").detach();
+            }
         } else {
-            action = "add"
+            var el = $(this);
+            var toggleClass = el.hasClass("checked");
+            $("ul.category:eq(0) a.checked").removeClass("checked");
+            if(!toggleClass) {
+                el.attr("href", el.attr("path-cat"));
+                el.addClass("checked");
+            } else {
+                el.attr("href", el.attr("path-all"));
+            }
         }
 
         var param = "?category_action=" + action + "&category=" + encodeURIComponent($(this).attr("value"));
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,19 +16,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= session[:current_user_id] && User.where(:_id => session[:current_user_id]).first
   end
 
-  def current_categories(action = nil, category = nil)
+  def current_categories
     @current_categories = session[:current_categories].to_a
     @current_categories = current_user.preferences if @current_categories.blank?
-    unless category.blank?
-      case action
-        when 'delete'
-          @current_categories.delete category
-        when 'add'
-          @current_categories << category unless @current_categories.include? category
-      end
-      session[:current_categories] = @current_categories
-    end
-    @current_categories.to_a
+    session[:current_categories] = @current_categories
   end
 
   def current_tags(action = nil, tag = nil)
@@ -61,7 +52,7 @@ class ApplicationController < ActionController::Base
 =end
 
   def categories
-    @categories ||= ['数码', '户外运动', '男装', '女装', '箱包','饰品', '化妆品', '居家', '食品','汽车','图书影像','创意礼品'  ]
+    @categories ||= ['数码', '户外运动', '男装', '女装', '箱包', '饰品', '化妆品', '居家', '食品', '汽车', '图书影像', '创意礼品']
   end
 
   def authenticate
@@ -88,13 +79,14 @@ class ApplicationController < ActionController::Base
 
     sns_type ||= (session[:sns_type] || params[:type])
     case sns_type
-    when 'sina'
-    unless is_sina_app_key_load?
-      put_sina_config_into_session load_sina_config
-    end
+      when 'sina'
+        #production sina app key
+        unless is_sina_app_key_load?
+          load_sina_config
+        end
 
-    @client ||= WeiboOAuth2::Client.new(session[:appkey], session[:appsecret])
-    WeiboOAuth2::Config.redirect_uri = session[:callback]
+        @client ||= WeiboOAuth2::Client.new(session[:appkey], session[:appsecret])
+        WeiboOAuth2::Config.redirect_uri = session[:callback]
 
     if !@client.authorized? && !session[:access_token].nil?
         @client.get_token_from_hash(:access_token => session[:access_token], :refresh_token => session[:refresh_token], :expires_at => session[:expires_at])
@@ -113,7 +105,10 @@ class ApplicationController < ActionController::Base
   end
 
   def load_sina_config
-    YAML.load_file(Rails.root.join("config/oauth","sina.yml"))[Rails.env]
+    sina = YAML.load_file(Rails.root.join("config/oauth", "sina.yml"))[Rails.env]
+    session[:appkey] = sina["key"]
+    session[:appsecret] = sina["secret"]
+    session[:callback] = sina["callback"]
   end
 
   def put_sina_config_into_session(sina)
