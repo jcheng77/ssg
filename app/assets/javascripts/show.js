1eb330743ec394373bc031f73438346fac61ee7c
reason <reasonlee@gmail.com>
2012-10-21 23:37:16 +0800

    item detail page refine

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-	$('.showitem-page .thumbnail').on({
-        mouseover:function () {
-        	var horPos = $(this).width()/2 - $('.item_actions').width()/2;
-        	$('.item_actions').css('left', horPos);
-            $(this).find('.item_actions').show();
-        },
-        mouseout:function () {
-            $(this).find('.item_actions').hide();
-        }
-    });
+    var calcActionListPosFunc = function(){
+            var verPos = $('.thumbnail').height() - $('.action-list').height() - 12;
+            var horPos = $('.thumbnail').width()/2 - $('.action-list').width()/2 - 20;
+            $('.action-list').css('top', verPos);
+            $('.action-list').css('left', horPos);
+        };
+
+    $('.showitem-page .thumbnail img').on({load: calcActionListPosFunc});
+    $(window).resize(calcActionListPosFunc);
 });
