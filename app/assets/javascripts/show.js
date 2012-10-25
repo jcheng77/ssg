efd8ba23141f354496ecd8dd667bfd19a3994953
reason <reasonlee@gmail.com>
2012-10-26 00:44:04 +0800

    minor fix on item detail page

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,11 +1,23 @@
 $(document).ready(function () {
+    
     var calcActionListPosFunc = function(){
-            var verPos = $('.thumbnail').height() - $('.action-list').height() - 12;
-            var horPos = $('.thumbnail').width()/2 - $('.action-list').width()/2 - 20;
-            $('.action-list').css('top', verPos);
-            $('.action-list').css('left', horPos);
-        };
+        var verPos = $('.thumbnail').height() - $('.count-list').height() - 12;
+        var horPos = $('.thumbnail').width()/2 - $('.count-list').width()/2 - 20;
+        $('.count-list').css('top', verPos);
+        $('.count-list').css('left', horPos);
+    };
 
     $('.showitem-page .thumbnail img').load(calcActionListPosFunc);
     $(window).resize(calcActionListPosFunc);
+
+    $('.thumbnail').on({
+        mouseover:function () {
+            var horPos = $(this).width()/2 - $('.item_actions').width()/2;
+            $('.item_actions').css('left', horPos);
+            $(this).find('.item_actions').show();
+        },
+        mouseout:function () {
+            $(this).find('.item_actions').hide();
+        }
+    });
 });
