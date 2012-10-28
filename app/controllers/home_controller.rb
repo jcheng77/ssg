2e7490ed8212a9919cd0bc1e8b9cf31bd93b6572
jcheng77 <jackie.f.cheng@gmail.com>
2012-10-29 01:02:18 +0800

    add before_filter for home controller to load empty nav

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
+  before_filter :select_empty_layout
+
   layout 'application'
   skip_before_filter :authenticate
+
 
   def index
     if current_user
