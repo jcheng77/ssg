c5271fdbc205fe6420366e10397f5de9aa5f2b09
jcheng77 <jackie.f.cheng@gmail.com>
2012-11-11 02:14:37 +0800

    make price optional

--- app/helpers/shares_helper.rb
+++ app/helpers/shares_helper.rb
@@ -13,4 +13,12 @@ module SharesHelper
     end
     image_tag share.item.image, class: html_class, title: html_title, type: share.share_type
   end
+
+  def price_tag(price)
+    if price == 0 || price.nil?
+      '暂无价格'
+    else
+      number_to_currency(price,unit: '￥')
+    end
+  end
 end
