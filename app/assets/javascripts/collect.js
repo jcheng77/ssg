a273ca0dfd2b7a7c00c3f462c44cb3f76fe44a77
jcheng77 <jackie.f.cheng@gmail.com>
2012-11-11 15:39:01 +0800

    remove debugger statement

--- app/assets/javascripts/collect.js
+++ app/assets/javascripts/collect.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $('#bookmarklet_container #collect-item').click(function () {
-    debugger;
     if ($('#item_category').find("option:selected").text() == '请选择') {
       alert('请选择商品分类');
       return false;
