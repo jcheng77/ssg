389242af560c0c4571b53591465050ba7f8e870e
reason <reasonlee@gmail.com>
2012-12-20 14:54:10 +0800

    nav refine for bootstrap responsive feature; bookmarlet entry dialog refine using bootstrap modal (notice: bootstrap has been updated to 2.2, do not merge it utill the feature is finished)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,23 +8,22 @@
 //= require jquery_ujs
 //= require jquery.easing.1.3
 //= require jquery.raty
-//= require jquery-tools/overlay/overlay
 //= require jquery-tools/toolbox/toolbox.expose
 //= require jquery-tools/tabs/tabs
 //
 // Include all twitter's javascripts
-//= require bootstrap
-//= require bootstrap-transition
-//= require bootstrap-alert
-//= require bootstrap-modal
-//= require bootstrap-dropdown
-//= require bootstrap-scrollspy
-//= require bootstrap-tab
-//= require bootstrap-tooltip
-//= require bootstrap-popover
-//= require bootstrap-button
-//= require bootstrap-collapse
-//= require bootstrap-carousel
-//= require bootstrap-typeahead
+//= require twitter/bootstrap
+//= require twitter/bootstrap/transition
+//= require twitter/bootstrap/alert
+//= require twitter/bootstrap/modal
+//= require twitter/bootstrap/dropdown
+//= require twitter/bootstrap/scrollspy
+//= require twitter/bootstrap/tab
+//= require twitter/bootstrap/tooltip
+//= require twitter/bootstrap/popover
+//= require twitter/bootstrap/button
+//= require twitter/bootstrap/collapse
+//= require twitter/bootstrap/carousel
+//= require twitter/bootstrap/typeahead
 //
 //= require_tree .
