05b9d5c8b3522df555cfbcdeeefe5acd7400baa8
jcheng <jackie.f.cheng@gmail.com>
2013-07-26 21:02:24 +0800

    add item title to the item page to increase the odds that the page is included in the releated google/baidu search result page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module ApplicationHelper
   def empty_link_if(condition, name)
     if condition
@@ -5,5 +7,9 @@ module ApplicationHelper
     else
       link_to name, yield
     end
+  end
+  
+  def title(page_title)
+    content_for :title, page_title
   end
 end
